c03b725ac522036da49d8d42e0849630fc2f2414
Greena1c <shchedrykserhii@gmail.com>
2024-12-30 22:20:18 +0200

    Sulution

---
+++ INSTRUCTION.md
@@ -0,0 +1,7 @@
+kubectl get secrets -n mysql
+kubectl get configmap -n mysql
+kubectl get headless -n mysql
+kubectl get pods -o wide -n mysql
+kubectl describe pod <name> -n mysql
+kubectl get ns
+kubectl logs <name> -n mysql
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+kind create cluster --config kind
+kubectl apply -f mysql-configmap.yaml
+kubectl apply -f mysql-secret.yaml
+kubectl apply -f pv.yaml
+kubectl apply -f pvc.yaml
+kubectl apply -f statefulset.yaml
+kubectl apply -f secret.yml
+kubectl apply -f deployment.yaml
